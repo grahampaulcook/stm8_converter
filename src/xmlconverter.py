be0e7fb84934d0150f6915819c55c9b2bf514fc2
grahampaulcook <grahampaulcook@users.noreply.github.com>
2023-01-25 18:58:22 +0000

    WIP

--- src/xmlconverter.py
+++ src/xmlconverter.py
@@ -1,16 +1,32 @@
 import xml.etree.ElementTree as ET
 
-root = ET.parse("STM8AL3148.xml")
+root = ET.parse("STM8S003F3.xml")
 
-for elem in root.findall(".//module[@name='BEEP']//bitfield"):
-    bitfieldName = elem.attrib.get('name')
-    bitfieldBits = elem.attrib.get('bits')
+for module in root.findall(".//module"):
+    modulename = module.attrib.get("name")
 
-    bitfieldBitsSplit = bitfieldBits.split("-")
+    for sfr in module.findall(".//SFR"):
+        address = sfr.attrib.get("address")  # .replace("0x", "$")
 
-    if len(bitfieldBitsSplit) == 1:
-        print(bitfieldName + bitfieldBits)
-    else:
-        # build the bitfield range
-        for xx in range(int(bitfieldBitsSplit[0]), int(bitfieldBitsSplit[1])):
-            print( bitfieldName + str(xx) )
+        for d in sfr.findall(".//register"):
+            description = d.attrib.get("description")
+            name = d.attrib.get("name")
+
+            print()
+            print(";" + description)
+            print(modulename + "_" + name + " EQU " + address)
+
+            for bitfield in sfr.findall(".//bitfield"):
+                bitfieldName = bitfield.attrib.get('name')
+                bitfieldBits = bitfield.attrib.get('bits')
+
+                bitfieldBitsSplit = bitfieldBits.split("-")
+
+                if len(bitfieldBitsSplit) == 1:
+                    print(bitfieldName + " EQU " + bitfieldBitsSplit[0])
+                else:
+                    # build the bitfield range
+                    tmp = int(0)
+                    for xx in range(int(bitfieldBitsSplit[0]), int(bitfieldBitsSplit[1]) + 1):
+                        print(bitfieldName + str(tmp) + " EQU " + str(xx))
+                        tmp += 1
